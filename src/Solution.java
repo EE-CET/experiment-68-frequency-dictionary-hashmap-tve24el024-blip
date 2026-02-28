2030f60d3fe25ac72c588c7b2b834c40bf05c289
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-28 22:43:37 +0530

    added

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,22 @@
-import java.util.HashMap;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+
+        String[] words = line.split(" ");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+}import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
